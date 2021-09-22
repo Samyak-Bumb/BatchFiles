c4d2bbe0272db813faa35449ca0a02dda61ba381
Samyak-Bumb <vijaybumb0575@gmail.com>
2021-09-22 18:47:12 +0530

    Add files via upload

---
+++ Drive Defragment.bat
@@ -0,0 +1,7 @@
+:: Written By Samyak Bumb
+:: Note If You Are Having SSD  Don't Defrag your PC *Important*
+@echo off
+defrag.exe c: -f
+defrag.exe d: -f
+:: If You Are Having 'e' Drive You Can Uncomment Below Line
+:: defrag.exe e: -f
---
+++ Own Command Prompt.bat
@@ -0,0 +1,10 @@
+:: Written By Samyak Bumb
+@echo off
+cls
+title Command Prompt - By Samyak Bumb
+ver
+echo Copyright (c) Samyak Bumb
+echo Welcome Samyak To Your Command Prompt
+echo.
+:cmd
+set /p "cmd=%cd%>"
